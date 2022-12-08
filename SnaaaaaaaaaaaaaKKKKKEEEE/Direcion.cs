ff88b178c973cddcccc6651d4e77bd7a1d35b726
Jabejixo <Ivanzv14007@gmail.com>
2022-12-08 23:37:41 +0300

    Добавьте файлы проекта.

---
+++ SnaaaaaaaaaaaaaKKKKKEEEE/Direcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnaaaaaaaaaaaaaKKKKKEEEE
+{
+    public enum Direction // Тут я преисполнился и понял насколько вещь полезная, хотя может мне кажется
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+}
